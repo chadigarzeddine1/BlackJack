ec7030dc27a33b3b3aad6448fb6d16debae48569
chadi.garzeddine1 <chadi.garzeddine1@zone33-ee.cs.ucalgary.ca>
2020-02-03 13:48:46 -0700

    Talking Pelham 123

---
+++ Chadi/src/Chadi.java
@@ -0,0 +1,25 @@
+
+public class Chadi {
+
+		public static boolean charsMatch(char lowercase, char uppercase) {
+			return 'a' <= lowercase && lowercase <= 'z' && 'A' <= uppercase && uppercase <= 'Z';
+					
+				
+			
+			
+			
+		}
+		public static long roundUp(double num) {
+			return (long)num;
+		}
+		public static int countAll(String str, String chars) {
+			return 0;
+		}
+		public static int addHexDigits(int num) {
+			int result = (int)num + num;
+			return result;
+		}
+
+	}
+
+}
