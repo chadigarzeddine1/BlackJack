a1071c68c2a987ab9a763e34a0318401938e9542
Andrew <7772172+AndrewB6@users.noreply.github.com>
2020-02-20 22:27:54 -0700

    Implemented in Card and Deck

--- CPSC233/src/Card.java
+++ CPSC233/src/Card.java
@@ -1,4 +1,29 @@
 
 public class Card {
-
+	private String value;
+	private String suit;
+	
+	public Card(String value, String suit) {
+		this.value = value;
+		this.suit = suit;
+	}
+	
+	public int getNumber() {
+		if (this.value.equals("A")) {
+			return 11;
+		} else if (this.value.equals("J") || this.value.equals("Q") || this.value.equals("K")) {
+			return 10;
+		} else {
+			return Integer.parseInt(this.value);
+		}
+		//return "♥, ♦, ♣, or ♠ ";
+	}
+	
+	public String getCard() {
+		return value + suit;
+	}
+	
+	public String toString() {
+		return getCard();
+	}
 }
--- CPSC233/src/Deck.java
+++ CPSC233/src/Deck.java
@@ -1,4 +1,39 @@
+import java.util.ArrayList;
 
 public class Deck {
-
+	private ArrayList<Card> cards;
+	
+	public Deck() {
+		this.cards = new ArrayList<Card>();
+		String[] suits = {"♥", "♦", "♣","♠"};
+		for (String suit: suits) {
+			for (int i = 0; i < 13; i++) {
+				Card newCard;
+				if (i == 1) {
+					newCard = new Card("A", suit);
+				} else if (i == 11) {
+					newCard = new Card("J", suit);
+				} else if (i == 12) {
+					newCard = new Card("Q", suit);
+				} else if (i == 13) {
+					newCard = new Card("K", suit);
+				} else {
+					newCard = new Card("" + i, suit);
+				}
+				cards.add(newCard);
+			}
+		}
+		
+	}
+	
+	public void shuffle() {
+		
+	}
+	
+	public Card draw() {
+		int random = (int )(Math.random() * cards.size());
+		Card returnCard = cards.get(random);
+		cards.remove(random);
+		return returnCard;
+	}
 }
