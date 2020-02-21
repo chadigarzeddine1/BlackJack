70b9236ff351d59b8ed4b3312313a5be7cf0e8ed
Andrew <7772172+AndrewB6@users.noreply.github.com>
2020-02-20 22:48:09 -0700

    player class update

--- CPSC233/src/Game.java
+++ CPSC233/src/Game.java
@@ -7,63 +7,64 @@ public class Game {
 	private ArrayList<Player> players = new ArrayList<Player>();
 	private PlayerAI dealer;
 	private Deck deck;
-	
+
 	//Asks user for number of players and then adds that many players to the players list plus the dealer player
 	public void start(ArrayList<Player> x) {
+		
 		if (x.isEmpty()) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter number of players:");
-		String numPlayers = input.nextLine();
-		int num = Integer.parseInt(numPlayers);
-		for (int i = 0; i < num; i++) {
-			Player p = new Player();
-			players.add(p);
-		}
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter number of players:");
+			String numPlayers = input.nextLine();
+			int num = Integer.parseInt(numPlayers);
+			for (int i = 0; i < num; i++) {
+				Player p = new Player();
+				players.add(p);
+			}
 		}
 		else {
 			players = x;
 		}
 		deck.shuffle();
-		
+
 		deal();	
 		while (allPlayersStand() == false) {
-		for (Player p :players) {
-			String play = p.go();
-			if (play == "Hit") {
-				hit(p);}
-		}
-		ArrayList<Player> win = bestHand(); 
-		Card c1 = deck.draw();
-		Card c2 = deck.draw();
-		this.dealer.setHand(c1);
-		this.dealer.setHand(c2);
-		while (dealer.getIsStanding() == false) {
-			String play = dealer.go();
-			if (play == "Hit") {
-				hit(dealer);}
-		}
-		Player p = win.get(0);
-		if (p.sum() > dealer.sum()) {
-			System.out.println(win+"Has won the round");
-		}
-		else if (p.sum()  == dealer.sum()) {
-			System.out.println(win+"Has tied with the dealer");
-		}
-		else if (p.sum() < dealer.sum()) {
-			System.out.println("The dealer has won the round");
-		}
-		Scanner inputt = new Scanner(System.in);
-		System.out.println("Play another round? Y/N");
-		String answer = inputt.nextLine();
-		if (answer == "Y") {
-			start(players);
-		}
-		else {
-			System.out.println("The richest player was" + richestplayer());
-		}
+			for (Player p :players) {
+				String play = p.go();
+				if (play == "Hit") {
+					hit(p);}
+			}
+			ArrayList<Player> win = bestHand(); 
+			Card c1 = deck.draw();
+			Card c2 = deck.draw();
+			this.dealer.setHand(c1);
+			this.dealer.setHand(c2);
+			while (dealer.getIsStanding() == false) {
+				String play = dealer.go();
+				if (play == "Hit") {
+					hit(dealer);}
+			}
+			Player p = win.get(0);
+			if (p.sum() > dealer.sum()) {
+				System.out.println(win+"Has won the round");
+			}
+			else if (p.sum()  == dealer.sum()) {
+				System.out.println(win+"Has tied with the dealer");
+			}
+			else if (p.sum() < dealer.sum()) {
+				System.out.println("The dealer has won the round");
+			}
+			Scanner inputt = new Scanner(System.in);
+			System.out.println("Play another round? Y/N");
+			String answer = inputt.nextLine();
+			if (answer == "Y") {
+				start(players);
+			}
+			else {
+				System.out.println("The richest player was" + richestplayer());
+			}
 		}
 	}
-	
+
 	//takes each player and deals them 2 cards
 	public void deal() {
 		for (Player p :players) {
@@ -73,19 +74,19 @@ public class Game {
 			p.setHand(c2);
 		}
 	}
-	
-	
+
+
 	// if the player is not standing will be dealt another card
 	public void hit(Player p) {
 		if (p.getIsStanding() == true) {
 			System.out.println("This player is already standing and can't hit.");
 		}
 		else {
-		Card c = deck.draw();
-		p.setHand(c);
+			Card c = deck.draw();
+			p.setHand(c);
 		}
 	}
-	
+
 	//goes through all the players and returns the players with the best hand
 	public ArrayList<Player> bestHand() {
 		ArrayList<Player> highest = new ArrayList<Player>();
@@ -103,7 +104,7 @@ public class Game {
 		}
 		return highest;
 	}
-	
+
 	//goes through each player in the game and checks if there isStanding variable is true or not.
 	public boolean allPlayersStand() {
 		boolean result = true;
@@ -114,8 +115,8 @@ public class Game {
 		}
 		return result;
 	}
-	
-	
+
+
 	public Player richestplayer() {
 		Player rich = new Player();
 		for (Player p : players) {
@@ -125,13 +126,13 @@ public class Game {
 		}
 		return rich;
 	}
-	
+
 	public static void main (String []args) {
 		Game g = new Game();
 		ArrayList<Player> y = new ArrayList<Player>();
 		g.start(y);
 	}
-	
-	
-	
+
+
+
 }
--- CPSC233/src/Player.java
+++ CPSC233/src/Player.java
@@ -102,4 +102,3 @@ public class Player {
         return bust;
 	}
 }
-}
